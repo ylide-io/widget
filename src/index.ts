13b90acc5c0dad82916f88edb6491dcd312a8c9e
Oleg Cherr <olegcherr@gmail.com>
2023-03-14 13:56:31 +0100

    Basic setup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// noinspection JSUnusedGlobalSymbols
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Ylide = {
+	openSendMessagePopup: (address: string) => {
+		console.log(333, address);
+	},
+};
